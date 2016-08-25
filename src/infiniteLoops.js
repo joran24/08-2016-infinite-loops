fc521e2d1b2f8f653873e753ef666b0807f9eab9
McKensie Mack <mckensiemack@gmail.com>
2016-08-24 23:11:05 -0500

    Passed up to challenge 6 in sprint

--- src/infiniteLoops.js
+++ src/infiniteLoops.js
@@ -61,22 +61,32 @@ var largestNumber = function(listOfNumbers){
 var nums = [1, 21, 471, 470, 39, 10, 92];
 
   var largestNum = largestNumber(nums);
-  console.log('This function checks to see what the largest number is in this array [1, 21, 471, 470, 39, 10, 92]'largestNum); //471
-
-
-	 
-
-
-
-//return largestNumber;
-
-
-
+  console.log('This function checks to see what the largest number is in this array [1, 21, 471, 470, 39, 10, 92], and that number is:', largestNum); //471
 
 // 5. multBy
-
-// 6. squareBy 
-
+var multBy = function(array, number) {
+	var newArray = [];
+	loop(array, function(element){
+		var multiplyNumber = number * element
+		newArray.push(multiplyNumber)
+	}) 
+	return newArray;
+	};
+  var nums = [1, 2, 3, 4, 5];
+  var multipliedArray = multBy(nums, 3);
+  console.log('This function takes in the number 3 and multiplies it by the numbers in the array [1, 2, 3, 4, 5] and outputs:', multipliedArray); // [3, 6, 9, 12, 15];
+// 6. squareOf <-- THIS IS WRONG
+var powerOf = function (array, number){
+	var newArray =[];
+	loop(array, function(element){
+		var raisedToPow = Math.pow(element, element);
+		newArray.push(raisedToPow)
+	})
+	return newArray;
+}; 
+  var nums = [1, 2, 3, 4, 5];
+  var newArray = powerOf(nums, nums);
+  console.log('This function takes a number and raises it by its own power. The output should be [1, 8, 27, 256, 3125]:', newArray) // [1, 8, 27, 256, 3125];
 // 7. capitalizeFirstLetters 
 
 // 8. collectValues 
